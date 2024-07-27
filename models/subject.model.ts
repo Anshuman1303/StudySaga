58e0f6f2c5662ef4d5783e2e50632b10cbb1fe82
kushagra-kiyawat <kushagra.kiyawat2@gmail.com>
2024-07-27 11:55:49 +0530

    add subject model

---
+++ models/subject.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Model } from "mongoose";
+
+export interface ISubject {
+    subject_name: string;
+    subject_id: string;
+    artwork_id: string;
+}
+
+export interface ISubjectDocument extends ISubject, Document {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const subjectSchema = new mongoose.Schema<ISubjectDocument>(
+    {
+        subject_name: {
+            type: String,
+            required: true,
+        },
+        subject_id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        artwork_id: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Subject: Model<ISubjectDocument> =
+    mongoose.models?.Subject ||
+    mongoose.model<ISubjectDocument>("Subject", subjectSchema);
