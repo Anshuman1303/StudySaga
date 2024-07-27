7fe2a170f5940459012faed76545690b49adc8a5
Anshuman Garg <anshuman.garg1303@gmail.com>
2024-07-27 18:40:52 +0530

    leaderboard UI changes

--- src/app/subject/[name]/leaderboard/page.tsx
+++ src/app/subject/[name]/leaderboard/page.tsx
@@ -1,27 +1,27 @@
-import Leaderboard from '@/components/Leaderboard'
-import connectToDB from '@/app/lib/db'
-import { Student } from '../../../../../models/student.model'
-import { Subject } from '../../../../../models/subject.model'
+import Leaderboard from "@/components/Leaderboard";
+import connectToDB from "@/app/lib/db";
+import { Student } from "../../../../../models/student.model";
+import { Subject } from "../../../../../models/subject.model";
+import { Title } from "@mantine/core";
 
-const LeaderboardComp = async ({ params }: { params: { name: string}}) => {
-	const subject_name = params.name;
-	const loggedInUser = "Hannah Ivy"
-	try {
-		await connectToDB();
-		const subject = await Subject.find({subject_name: subject_name})
-		const students = await Student.find({ 'subjects_with_exp.subject': subject[0]._id }).populate('subjects_with_exp.subject');
-		console.log(students);
-		// students.sort((a, b) => b.subjectExp[b.subjects.indexOf(subject)] - a.subjectExp[a.subjects.indexOf(subject)]);
-		const s = JSON.parse(JSON.stringify(students));
-		return (
-			<Leaderboard data={s} loggedInUser={loggedInUser} subject={subject_name}></Leaderboard>
-	  )
-	}
-	catch {
-		return (
-			<div>Some error occurred.</div>
-		)
-	}
-}
+const LeaderboardComp = async ({ params }: { params: { name: string } }) => {
+  const subject_name = params.name;
+  const loggedInUser = "Hannah Ivy";
+  try {
+    await connectToDB();
+    const subject = await Subject.find({ subject_name: subject_name });
+    const students = await Student.find({ "subjects_with_exp.subject": subject[0]._id }).populate("subjects_with_exp.subject");
+    console.log(students);
+    // students.sort((a, b) => b.subjectExp[b.subjects.indexOf(subject)] - a.subjectExp[a.subjects.indexOf(subject)]);
+    const s = JSON.parse(JSON.stringify(students));
+    return (
+      <>
+        <Leaderboard data={s} loggedInUser={loggedInUser} subject={subject_name}></Leaderboard>
+      </>
+    );
+  } catch {
+    return <div>Some error occurred.</div>;
+  }
+};
 
-export default LeaderboardComp
+export default LeaderboardComp;
--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,34 +1,35 @@
-"use client"
-import { Container, Stack } from '@mantine/core';
-import { useRef, useEffect } from 'react';
-import { IStudentDocument } from '../../models/student.model';
+"use client";
+import { Center, Container, Flex, Stack, Title } from "@mantine/core";
+import { useRef, useEffect } from "react";
+import { IStudentDocument } from "../../models/student.model";
 interface LeaderboardProps {
-	data: IStudentDocument[],
-	loggedInUser: string,
-	subject: string
+  data: IStudentDocument[];
+  loggedInUser: string;
+  subject: string;
 }
 const Leaderboard: React.FC<LeaderboardProps> = ({ data, loggedInUser, subject }) => {
-	const refs = useRef<(HTMLDivElement | null)[]>([]);
-	useEffect(() => {
-		const targetIndex = data.findIndex(item => item.firstName + ' ' + item.lastName === loggedInUser);
-		if (targetIndex !== -1 && refs.current[targetIndex]) {
-		  refs.current[targetIndex].scrollIntoView({ behavior: 'smooth', block: 'end' });
-		}
-	  }, [data, loggedInUser]);
-	  const setRef = (index: number) => (el: HTMLDivElement | null) => {
-		refs.current[index] = el;
-	  };
+  const refs = useRef<(HTMLDivElement | null)[]>([]);
+  useEffect(() => {
+    const targetIndex = data.findIndex((item) => item.firstName + " " + item.lastName === loggedInUser);
+    if (targetIndex !== -1 && refs.current[targetIndex]) {
+      refs.current[targetIndex].scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  }, [data, loggedInUser]);
+  const setRef = (index: number) => (el: HTMLDivElement | null) => {
+    refs.current[index] = el;
+  };
   return (
-	<Stack
-		m={"0 auto"}
-		w='90%'
-		align='stretch'
-		justify='flex-start'
-		gap={0}
-	>
-		{data.map((item, index) => <Container key={index} h={30} bg={index % 2 == 0 ? "#DEDEDE" : "#FFFFFF"}  ref={setRef(index)} w={'100%'} fluid>{index + 1} : {item.firstName} {item.lastName}</Container>)}
-	</Stack>
-  )
-}
+    <Flex w="100%" py="xl" direction="column" align="center" gap="md">
+      <Title order={1}>Leaderboard</Title>
+      <Stack w="90%" align="stretch" justify="flex-start" gap={0}>
+        {data.map((item, index) => (
+          <Container key={index} h={30} bg={index % 2 == 0 ? "#DEDEDE" : "#FFFFFF"} ref={setRef(index)} w={"100%"} fluid>
+            {index + 1} : {item.firstName} {item.lastName}
+          </Container>
+        ))}
+      </Stack>
+    </Flex>
+  );
+};
 
-export default Leaderboard
+export default Leaderboard;
